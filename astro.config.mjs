c2a142496b93c7db641b0254a903ed9b3b6a7a42
Lucas <lucasdourado.d@gmail.com>
2026-03-22 14:05:45 -0300

    feat: lançamento da plataforma LexLab v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-                            // NÃO deve ter a linha 'base: "/lexlab"' se você quer usar localhost:4321/
+  output: 'static', // Garante que o Astro gere apenas arquivos HTML/JS/CSS
 });
